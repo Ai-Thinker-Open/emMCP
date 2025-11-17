1c9c5540af521717f84dac979f9a662b09b67426
Seahi <seahi-mo@foxmail.com>
2025-11-17 16:46:11 +0800

    Refactor GPIO and SPI configurations, remove unused timer, and implement newlib locking mechanism
    - Updated GPIO pin definitions for OLED display in main.h to use GPIOA instead of GPIOB.
    - Removed unused timer definitions and callback functions related to emMCP_TickHandleTimers in freertos.c.
    - Adjusted GPIO initialization in gpio.c to set correct output levels and configurations.
    - Changed SPI baud rate prescaler from 64 to 16 in spi.c for improved communication speed.
    - Updated STM32CubeMX configuration to remove unused timer settings and adjust pin configurations.
    - Added new locking mechanism for newlib in newlib_lock_glue.c and defined locking strategies in stm32_lock.h.
    - Switched OpenOCD configuration from cmsis-dap to stlink for better compatibility.
    - Deleted obsolete image files from STM32F40xRTOS_XiaoZhiAI documentation.

--- example/STM32F10xRTOS_XiaoZhiAI/Core/Inc/main.h
+++ example/STM32F10xRTOS_XiaoZhiAI/Core/Inc/main.h
@@ -62,12 +62,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define OLED_DC_Pin GPIO_PIN_0
+#define OLED_DC_GPIO_Port GPIOA
+#define OLED_CS2_Pin GPIO_PIN_1
+#define OLED_CS2_GPIO_Port GPIOA
 #define OLED_CS1_Pin GPIO_PIN_4
 #define OLED_CS1_GPIO_Port GPIOA
-#define OLED_CS2_Pin GPIO_PIN_0
-#define OLED_CS2_GPIO_Port GPIOB
-#define OLED_DC_Pin GPIO_PIN_1
-#define OLED_DC_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- example/STM32F10xRTOS_XiaoZhiAI/Core/Src/freertos.c
+++ example/STM32F10xRTOS_XiaoZhiAI/Core/Src/freertos.c
@@ -54,11 +54,6 @@ const osThreadAttr_t defaultTask_attributes = {
   .stack_size = 256 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
-/* Definitions for emMCP_TickHandleTimers */
-osTimerId_t emMCP_TickHandleTimersHandle;
-const osTimerAttr_t emMCP_TickHandleTimers_attributes = {
-  .name = "emMCP_TickHandleTimers"
-};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -80,7 +75,6 @@ void oled_TickHandle_taskHandle(void *argument);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
-void emMCP_TickHandleTimersCallback(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -101,10 +95,6 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_SEMAPHORES */
   /* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
-
-  /* Create the timer(s) */
-  /* creation of emMCP_TickHandleTimers */
-  emMCP_TickHandleTimersHandle = osTimerNew(emMCP_TickHandleTimersCallback, osTimerPeriodic, NULL, &emMCP_TickHandleTimers_attributes);
 
   /* USER CODE BEGIN RTOS_TIMERS */
   /* start timers, add new ones, ... */
@@ -146,14 +136,6 @@ void StartDefaultTask(void *argument)
      emMCP_TickHandle(10);
   }
   /* USER CODE END StartDefaultTask */
-}
-
-/* emMCP_TickHandleTimersCallback function */
-void emMCP_TickHandleTimersCallback(void *argument)
-{
-  /* USER CODE BEGIN emMCP_TickHandleTimersCallback */
-   
-  /* USER CODE END emMCP_TickHandleTimersCallback */
 }
 
 /* Private application code --------------------------------------------------*/
